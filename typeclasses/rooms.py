9f32fd143c453f02b856fb24a7b5c8c64c14b71d
Dale Lemieux <dale.lemieux@gmail.com>
2018-05-06 19:17:09 -0700

    People can go in a circle on the ride!

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -11,6 +11,7 @@ import datetime
 from evennia import CmdSet, Command
 from evennia import create_object
 from evennia import DefaultRoom
+from evennia import DefaultExit
 from evennia import TICKER_HANDLER
 
 class Room(DefaultRoom):
@@ -283,8 +284,6 @@ class CmdSetBoardingZone(CmdSet):
 
 
 class ChimeraBoardingZone(DefaultRoom):
-    # Have the room destroy itself if no one ever shows up after 2 minutes, or if everyone leaves the room
-
     task_messages = [
         "You may talk amongst yourselves while I verify the integrity of your restraints...",
         "Please entertain each other and pay no mind to the loose bolts I'll be fixing with this wrench...",
@@ -345,7 +344,7 @@ class ChimeraBoardingZone(DefaultRoom):
             return 9 # Slight delay with no text for people to enter
         elif index == 1:
             msg = ""
-            msg += "-----------------------------------------" + "\n"
+            msg += "|y> auto-advance|n" + "\n"
             msg += "Welcome to the boarding zone!" + "\n"
             msg += "Just get in your seats and the ride will begin shortly." + "\n"
 
@@ -355,7 +354,7 @@ class ChimeraBoardingZone(DefaultRoom):
             return 9
         elif index == 2:
             msg = ""
-            msg += "-----------------------------------------" + "\n"
+            msg += "|y> auto-advance|n" + "\n"
             msg += "A massive chimera approaches from beneath the cart and you feel the entire thing raise up!"
 
             self.msg_contents(msg)
@@ -363,7 +362,7 @@ class ChimeraBoardingZone(DefaultRoom):
             return 9
         elif index == 3:
             msg = ""
-            msg += "-----------------------------------------" + "\n"
+            msg += "|y> auto-advance|n" + "\n"
             msg += "As the chimera flexes its legs, you are tossed about easily like toy." + "\n"
             msg += "Suddenly...the chimera leaps forward! And you're off!"
 
@@ -375,4 +374,104 @@ class ChimeraBoardingZone(DefaultRoom):
         
 
 class ChimeraRideRoom(DefaultRoom):
-    pass
+    def at_object_creation(self):
+        super(ChimeraRideRoom, self).at_object_creation()
+
+        desc = ""
+        desc += "You are now on the ride."
+        self.db.desc = desc
+
+        #self.cmdset.add_default(CmdSetBoardingZone)
+
+        self.db.interval = 1 # Every X seconds it updates the room
+        TICKER_HANDLER.add(interval=self.db.interval, callback=self.update_loop, idstring="the_ride")
+
+
+    def update_loop(self):
+        now = datetime.datetime.utcnow()
+
+        if not hasattr(self, 'last_event_time'):
+            self.last_event_time = now
+
+        if not hasattr(self, 'msg_index'):
+            self.msg_index = -1
+
+        if not hasattr(self, 'msg_delay'):
+            self.msg_delay = 0
+
+        elapsed = now - self.last_event_time
+        if elapsed.seconds > self.msg_delay:
+            self.msg_index = self.msg_index + 1 # Advance to the next index
+
+            next_delay = self.send_message(self.msg_index)
+            if next_delay >= 0:
+                self.msg_delay = next_delay
+                self.last_event_time = now
+            else: # Nothing left to do in here
+                if len(self.contents) > 0:
+                    # Create a room for the exit
+                    new_exit_room = create_object(ChimeraExitRoom, key="Chimera Ride Exit Room")
+
+                    # TODO: Create a photo item for everyone and add it to their inventory
+                    # Make sure the description lists who was in the photo
+
+                    # Move all players into the ride room
+                    for rider in self.contents:
+                        rider.move_to(new_exit_room)
+
+                # Destroy this room
+                self.delete()
+
+
+    # Returns the number of seconds to wait until the next event
+    def send_message(self, index):
+        if index == 0:
+            return 9 # Slight delay with no text for people to enter
+        elif index == 1:
+            msg = ""
+            msg += "|y> auto-advance|n" + "\n"
+            msg += "First ride event"
+
+            self.msg_contents(msg)
+
+            return 9
+        elif index == 2:
+            msg = ""
+            msg += "|y> auto-advance|n" + "\n"
+            msg += "Second ride event"
+
+            self.msg_contents(msg)
+
+            return 9
+        elif index == 3:
+            msg = ""
+            msg += "|y> auto-advance|n" + "\n"
+            msg += "Third ride event"
+
+            self.msg_contents(msg)
+
+            return 9
+
+        return -1
+
+class ChimeraExitRoom(DefaultRoom):
+    def at_object_creation(self):
+        super(ChimeraExitRoom, self).at_object_creation()
+
+        desc = ""
+        desc += "Thank you for riding!\n"
+        desc += "If you check your |ginventory|n you will see the photo that was taken."
+        desc += "You have also all been given 2 points for riding with us today!"
+
+        # Set up an exit in the room that they can take
+        gift_shop_room = "#228" #self.search("ChimeraGiftShop")
+
+        typeclass = "typeclasses.exits.Exit"
+        exit_obj = create_object(typeclass, "gift shop", self, aliases=["gift","shop"], destination=gift_shop_room)
+        exit_obj.db.desc = "The way to the gift shop."
+
+        self.db.desc = desc
+
+    # TODO: Destroy this room when the last person leaves
+
+
